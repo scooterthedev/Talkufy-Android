299cdd2cb0144fc19ebf8131d7581ab542bb8f48
Scooter <>
2021-10-24 13:29:09 -0400

    Added googleservices.json

--- app/build/generated/source/buildConfig/debug/ca/scooter/Talkufy/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/ca/scooter/Talkufy/BuildConfig.java
@@ -5,7 +5,7 @@ package ca.scooter.Talkufy;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "ca.scooter.Say_Talk";
+  public static final String APPLICATION_ID = "ca.scooter.Talkufy";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 5;
   public static final String VERSION_NAME = "1.0.5";
