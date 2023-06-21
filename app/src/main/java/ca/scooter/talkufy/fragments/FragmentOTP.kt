20a1bf191e4d2ce1dada8c1dd1484a25cdb73d65
Scooter Y <talkufy@outlook.com>
2023-06-21 14:49:30 -0400

    Fixed Bugs:
    - Fixed A High Severity Bug That Allows a malicious application to communicate with the jitsi audio broadcast receiver including when using bluetooth or wired headphones.
    - Fixed a bug were answering a video/audio request would crash the app.
    
    Feature Updates:
    
    - Added v1 of an app-updater which updates from GitHub servers.
    
    Current Bugs:
    
    - When answering a video/audio calling request it would join 2 meets instead of one with everyone in it.
    - Photo, and Taking a photo from the Attachment Menu will result in an app crash/restart.
    - Huge Screen Flicker when looking at the notification menu when in P&P mode.
    - A bug were when viewing a sent location, the app would not be able to display your current location.

---
+++ app/src/main/java/ca/scooter/talkufy/fragments/FragmentOTP.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9936ba67f35f8e7d673ae7159579a6562d747375e88588b6bbe0b9fc2204f46f
+size 9751
---
+++ app/src/main/java/ca/scooter/talkufy/jitsi_sdk/OngoingConferenceTracker.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b599b9c9b43132ecffc8d1ad5224c69841337a90208b603e48057cedf9f5ea9c
+size 2885
